e0751e8389ab5c296a6f5ba347a3795a7339c31b
iRebeca <birleanurebeca@yahoo.com>
2020-05-04 17:57:02 +0300

    some change

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.print("Optionui : 1. create  2.join 3.submit:ij , unde i este linia si j coloana ");
         GameClinet gameClinet=new GameClinet();
         try{gameClinet.communicate() ;}catch (IOException e){System.out.println(e.getMessage());}
     }
